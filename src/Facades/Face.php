ec0945666497ad7c76a24fd8fac33d1a7f3a10df
Jose Galindo <jgalindo@softlogic.com.gt>
2019-11-07 16:34:45 -0600

    Downgrade a 5.1

--- src/Facades/Face.php
+++ src/Facades/Face.php
@@ -1,7 +1,10 @@
-<?php namespace Csgt\Face\Facades;
- 
+<?php
+namespace Csgt\Face\Facades;
+
 use Illuminate\Support\Facades\Facade;
- 
-class Face extends Facade {
-  protected static function getFacadeAccessor() { return 'face'; }
+
+class Face extends Facade
+{
+    protected static function getFacadeAccessor()
+    {return 'face';}
 }
--- src/config/csgtface.php
+++ src/config/csgtface.php
@@ -1,5 +1,6 @@
 <?php
+
 return [
-	'testurl' => 'http://test.documentagface.com/mx.com.fact.wsfront/FactWSFront.asmx?wsdl',
-	'url'     => 'https://www.documentagface.com/mx.com.fact.wsfront/FactWSFront.asmx?wsdl',
+    'testurl' => 'http://test.documentagface.com/mx.com.fact.wsfront/FactWSFront.asmx?wsdl',
+    'url'     => 'https://www.documentagface.com/mx.com.fact.wsfront/FactWSFront.asmx?wsdl',
 ];
